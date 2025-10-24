7493e2df09bb189dad41779383e8ef90c6925e2e
Denis Kin <d3n0s4vr@gmail.com>
2025-10-24 21:14:11 +0400

    more practice

---
+++ 023-function-printNumbers/index.js
@@ -0,0 +1,20 @@
+/*
+Вывод каждую секунду
+важность: 5
+
+Напишите функцию printNumbers(from, to), которая выводит число каждую секунду, начиная от from и заканчивая to.
+
+Сделайте два варианта решения.
+
+    Используя setInterval.
+    Используя рекурсивный setTimeout.
+*/
+
+function printNumbers(from, to) {
+  let current = from;
+  let timerId = setInterval(() => {
+    alert(current);
+    if (current == to) clearInterval(timerId);
+    current++;
+  }, 1000);
+}
