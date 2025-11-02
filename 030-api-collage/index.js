30983cc49da266b4d1d8d45e223ef05f68eff885
Denis Kin <d3n0s4vr@gmail.com>
2025-11-02 20:25:08 +0400

    Создал мини-приложение с получением изображений с помощью API

---
+++ 030-api-collage/index.js
@@ -0,0 +1,87 @@
+// Первое задание
+// Необходимо отобразить коллаж с фотографиями животных:
+// 1. Получить изображения с помощью стороннего API https://dog.ceo/dog-api/
+// 2. Добавлять в коллаж новые изображения каждые 3 секунды
+// 3. Заменять старые изображения, если они не помеёщаются в контейнер или достигнут лимит изображений
+// 4. Открывать изображения в режиме просмотра
+// 5. Обновление коллажа приостанавливается, если изображение открыто в режиме просмотра
+// 6*. Добавить возможность скачивать изображения в режиме просмотра
+
+const images = [];
+const collage = document.getElementById("collage");
+const getAnimalImgBtn = document.getElementById("getAnimalImgBtn");
+let intervalId = null;
+
+function fetchAnimalImages() {
+  const url = "https://dog.ceo/api/breeds/image/random";
+  return fetch(url).then((res) => {
+    return res.json();
+  });
+}
+
+function renderImages() {
+  collage.innerHTML = "";
+  images.forEach((image, index) => {
+    const li = document.createElement("li");
+    li.innerHTML = `<img src=${image} height="150">`;
+    collage.appendChild(li);
+  });
+  const items = document.querySelectorAll("img");
+  items.forEach((item, index) => {
+    item.addEventListener("mouseover", () => {
+      item.style.transform = "scale(3)";
+      item.addEventListener("click", () => {
+        const a = document.createElement("a");
+        a.href = images[index];
+        a.download = "";
+        a.click();
+      });
+      toggleInterval(false);
+    });
+    item.addEventListener("mouseout", () => {
+      item.style.transform = "scale(1)";
+      toggleInterval(true);
+    });
+  });
+}
+
+function displayAnimalImage(data) {
+  let { message } = data;
+  for (let i = images.length - 1; i > 0; i--) {
+    images[i] = images[i - 1];
+  }
+  images[0] = message;
+  if (images.length >= 5) {
+    images.length = 5;
+  } else {
+    images.length = images.length + 1;
+  }
+
+  renderImages();
+}
+
+getAnimalImgBtn.addEventListener("click", () => {
+  fetchAnimalImages()
+    .then((data) => {
+      displayAnimalImage(data);
+    })
+    .catch((error) => {
+      throw new Error(error);
+    });
+  toggleInterval(true);
+});
+
+function toggleInterval(bool) {
+  if (bool) {
+    if (!intervalId) {
+      intervalId = setInterval(() => {
+        getAnimalImgBtn.click();
+      }, 3000);
+    }
+  } else {
+    if (intervalId) {
+      clearInterval(intervalId);
+      intervalId = null;
+    }
+  }
+}
